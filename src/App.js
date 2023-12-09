335eed21f0fc92cef2139bb8780883d05f2f4fc1
Mike DeAngelis <mikedeangelis@MikeMacbookPro.local>
2023-12-09 09:07:09 -0500

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from "./logo.svg";
-import "./App.css";
+// import "@aws-amplify/ui-react/styles.css";
+// import {
+//   withAuthenticator,
+//   Button,
+//   Heading,
+//   Image,
+//   View,
+//   Card,
+// } from "@aws-amplify/ui-react";
 
-function App() {
+import { Amplify } from "aws-amplify";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Live from v2
-        </a>
-      </header>
-    </div>
+    <Authenticator signUpAttributes={["email"]}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
